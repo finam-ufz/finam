9bd5088e6a07c9e5857c373e574a3eb19911d867
Martin Lange <martin.lange@ufz.de>
2021-06-21 12:35:30 +0200

    add test for adapter chaining

---
+++ finam/core/test_sdk.py
@@ -0,0 +1,30 @@
+import unittest
+
+from .sdk import AAdapter
+
+
+class DummyAdapter(AAdapter):
+    def __init__(self):
+        super().__init__()
+
+    def get_data(self, time):
+        return time
+
+
+class TestChaining(unittest.TestCase):
+    def test_chaining(self):
+        adapter1 = DummyAdapter()
+        adapter2 = DummyAdapter()
+        adapter3 = DummyAdapter()
+
+        adapter1 >> adapter2 >> adapter3
+
+        self.assertEqual(adapter1.targets, [adapter2])
+        self.assertEqual(adapter2.targets, [adapter3])
+
+        self.assertEqual(adapter2.source, adapter1)
+        self.assertEqual(adapter3.source, adapter2)
+
+
+if __name__ == "__main__":
+    unittest.main()
