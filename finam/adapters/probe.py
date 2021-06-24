a934cbd5e81bbac2190e595fb6b5ba0b69b7391b
Martin Lange <martin.lange@ufz.de>
2021-06-24 22:13:02 +0200

    add callback probe adapter

---
+++ finam/adapters/probe.py
@@ -0,0 +1,21 @@
+from core.sdk import AAdapter
+
+
+class CallbackProbe(AAdapter):
+    """
+    Probe data by calling a callback. Simply forwards the data unchanged.
+    """
+
+    def __init__(self, callback):
+        """
+        Create a new Callback generator.
+
+        :param callback: A callback ``callback(data, time)``, returning nothing.
+        """
+        super().__init__()
+        self.callback = callback
+
+    def get_data(self, time):
+        data = self.pull_data(time)
+        self.callback(data, time)
+        return data
