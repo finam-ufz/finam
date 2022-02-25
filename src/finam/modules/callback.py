1fb7470df871771b97b2d2f2a8645d27c7626003
Martin Lange <martin.lange@ufz.de>
2022-02-25 13:57:02 +0100

    add a generic callback component with arbitrary inputs and outputs (primarily use for for testing)

---
+++ src/finam/modules/callback.py
@@ -0,0 +1,81 @@
+"""Generic component based on a callback."""
+
+from datetime import datetime, timedelta
+
+from ..core.interfaces import ComponentStatus
+from ..core.sdk import ATimeComponent, Input, Output
+
+
+class CallbackComponent(ATimeComponent):
+    """
+    Component to generate, transform or consume data in fixed time intervals using a callback.
+    """
+
+    def __init__(self, inputs, outputs, callback, start, step):
+        """
+        Create a new CallbackComponent.
+
+        :param inputs: a list of input names
+        :param outputs: a list of output names
+        :param callback: a callback f({inputs}, time) -> {outputs}
+        :param start: start datetime
+        :param step: step timedelta
+        """
+        super().__init__()
+
+        if not isinstance(start, datetime):
+            raise ValueError("Start must be of type datetime")
+        if not isinstance(step, timedelta):
+            raise ValueError("Step must be of type timedelta")
+
+        self._input_names = inputs
+        self._output_names = outputs
+        self._callback = callback
+        self._step = step
+        self._time = start
+        self._status = ComponentStatus.CREATED
+
+    def initialize(self):
+        super().initialize()
+
+        for name in self._input_names:
+            self._inputs[name] = Input()
+
+        for name in self._output_names:
+            self._outputs[name] = Output()
+
+        self._status = ComponentStatus.INITIALIZED
+
+    def connect(self):
+        super().connect()
+
+        inp = {n: None for n in self._input_names}
+        outp = self._callback(inp, self.time)
+
+        for name, val in outp.items():
+            self._outputs[name].push_data(val, self.time)
+
+        self._status = ComponentStatus.CONNECTED
+
+    def validate(self):
+        super().validate()
+
+        self._status = ComponentStatus.VALIDATED
+
+    def update(self):
+        super().update()
+
+        inp = {n: self.inputs[n].pull_data(self.time) for n in self._input_names}
+
+        self._time += self._step
+
+        outp = self._callback(inp, self.time)
+        for name, val in outp.items():
+            self._outputs[name].push_data(val, self.time)
+
+        self._status = ComponentStatus.UPDATED
+
+    def finalize(self):
+        super().finalize()
+
+        self._status = ComponentStatus.FINALIZED
