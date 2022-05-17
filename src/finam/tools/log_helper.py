fb2fa5af44e7c866b09e366223a080c912b39e44
Sebastian Müller <mueller.seb@posteo.de>
2022-05-17 12:51:42 +0200

    log: add log context manager for low level C output

--- src/finam/tools/log_helper.py
+++ src/finam/tools/log_helper.py
@@ -3,6 +3,8 @@ import logging
 import sys
 from contextlib import AbstractContextManager
 
+import wurlitzer
+
 
 class LogWriter:
     """
@@ -64,3 +66,41 @@ class LogStdOutStdErr(AbstractContextManager):
     def __exit__(self, *args, **kwargs):
         setattr(sys, "stdout", self._old_stdout)
         setattr(sys, "stderr", self._old_stderr)
+
+
+class LogCStdOutStdErr:
+    """
+    Context manager to redirect low-level C stdout and stderr to a logger.
+
+    Parameters
+    ----------
+    logger_name : string or None, optional
+        Logger name for this writer. Will be the root logger by default.
+
+    level_stdout : integer, optional
+        Logging level for stdout, by default logging.INFO
+
+    level_stderr : integer, optional
+        Logging level for stderr, by default logging.WARN
+    """
+
+    def __init__(
+        self, logger_name=None, level_stdout=logging.INFO, level_stderr=logging.WARN
+    ):
+        self.logger_name = logger_name
+        self.level_stdout = level_stdout
+        self.level_stderr = level_stderr
+        self.stdout = None
+        self.stderr = None
+        self.pipes = wurlitzer.pipes()
+
+    def __enter__(self):
+        self.stdout, self.stderr = self.pipes.__enter__()
+
+    def __exit__(self, type, value, traceback):
+        self.pipes.__exit__(type, value, traceback)
+        logger = logging.getLogger(self.logger_name)
+        for line in self.stdout.read().splitlines():
+            logger.log(self.level_stdout, line)
+        for line in self.stderr.read().splitlines():
+            logger.log(self.level_stderr, line)
