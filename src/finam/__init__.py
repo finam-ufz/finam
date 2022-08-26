c30acca2636d85d2de545fcdde3a5a49dec09f9e
Sebastian Müller <mueller.seb@posteo.de>
2022-08-26 11:52:50 +0200

    coverage: exclude wurlitzer module

--- src/finam/__init__.py
+++ src/finam/__init__.py
@@ -6,6 +6,6 @@ from . import adapters, core, data, modules
 
 try:
     from ._version import __version__
-except ModuleNotFoundError:  # pragma: nocover
+except ModuleNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = "0.0.0.dev0"
