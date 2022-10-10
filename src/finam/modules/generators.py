b232ff2706a0c250b90678aa67a3f4faad91ccb6
Martin Lange <martin.lange@ufz.de>
2022-10-10 13:47:28 +0200

    fix connect procedure in callback generator

--- src/finam/modules/generators.py
+++ src/finam/modules/generators.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 from ..core.interfaces import ComponentStatus
 from ..core.sdk import ATimeComponent, Output
+from ..tools.connect_helper import ConnectHelper
 from ..tools.log_helper import LogError
 
 
@@ -39,6 +40,8 @@ class CallbackGenerator(ATimeComponent):
         self._callbacks = callbacks
         self._step = step
         self._time = start
+        self._connector = None
+        self._initial_data = None
         self.status = ComponentStatus.CREATED
 
     def initialize(self):
@@ -52,6 +55,8 @@ class CallbackGenerator(ATimeComponent):
         for key, (_, info) in self._callbacks.items():
             self._outputs[key] = Output(info)
 
+        self._connector = ConnectHelper(self.inputs, self.outputs)
+
         self.status = ComponentStatus.INITIALIZED
 
     def connect(self):
@@ -61,10 +66,22 @@ class CallbackGenerator(ATimeComponent):
         """
         super().connect()
 
-        for key, (callback, _) in self._callbacks.items():
-            self._outputs[key].push_data(callback(self._time), self.time)
+        if self._initial_data is None:
+            self._initial_data = {
+                key: callback(self._time)
+                for key, (callback, _) in self._callbacks.items()
+            }
 
-        self.status = ComponentStatus.CONNECTED
+        push_data = {}
+        for name, pushed in self._connector.data_pushed.items():
+            if not pushed:
+                push_data[name] = self._initial_data[name]
+
+        self.status = self._connector.connect(self._time, push_data=push_data)
+
+        if self.status == ComponentStatus.CONNECTED:
+            del self._initial_data
+            del self._connector
 
     def validate(self):
         """Validate the correctness of the component's settings and coupling.
