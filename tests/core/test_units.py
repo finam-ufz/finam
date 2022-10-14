move unit conversion to inputs, fix some checks

tests/core/test_units.py
@@ -6,10 +6,9 @@ from datetime import datetime, timedelta
 
 import numpy as np
 
-from finam.adapters.units import ConvertUnits
-from finam.core.interfaces import ComponentStatus
+from finam.core.interfaces import ComponentStatus, FinamMetaDataError
 from finam.core.schedule import Composition
-from finam.core.sdk import ATimeComponent, Input
+from finam.core.sdk import ATimeComponent
 from finam.data import Info, tools
 from finam.data.grid_spec import UniformGrid
 from finam.data.grid_tools import Location
@@ -83,7 +82,7 @@ class TestUnits(unittest.TestCase):
         composition = Composition([source, sink])
         composition.initialize()
 
-        (source.outputs["Output"] >> ConvertUnits() >> sink.inputs["Input"])
+        (source.outputs["Output"] >> sink.inputs["Input"])
 
         composition.run(t_max=datetime(2000, 1, 2))
 
@@ -92,3 +91,30 @@ class TestUnits(unittest.TestCase):
         )
         self.assertEqual(tools.get_units(sink.data), tools.UNITS.kilometer)
         self.assertEqual(tools.get_magnitude(sink.data)[0, 0, 0], 0.001)
+
+    def test_units_fail(self):
+        in_info = Info(
+            grid=UniformGrid(
+                dims=(5, 10), spacing=(2.0, 2.0, 2.0), data_location=Location.POINTS
+            ),
+            units=tools.UNITS.meter,
+        )
+
+        in_data = np.zeros(shape=in_info.grid.data_shape, order=in_info.grid.order)
+        in_data.data[0, 0] = 1.0
+
+        source = CallbackGenerator(
+            callbacks={"Output": (lambda t: in_data, in_info)},
+            start=datetime(2000, 1, 1),
+            step=timedelta(days=1),
+        )
+
+        sink = MockupConsumer(datetime(2000, 1, 1), tools.UNITS.seconds)
+
+        composition = Composition([source, sink])
+        composition.initialize()
+
+        (source.outputs["Output"] >> sink.inputs["Input"])
+
+        with self.assertRaises(FinamMetaDataError):
+            composition.run(t_max=datetime(2000, 1, 2))