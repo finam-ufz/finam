57812029e15c94ddff5ca8e6df37f5b50cead601
Martin Lange <martin.lange@ufz.de>
2022-10-16 22:53:51 +0200

    add tests for visual components

---
+++ tests/modules/visual/test_schedule_view.py
@@ -0,0 +1,32 @@
+import unittest
+from datetime import datetime, timedelta
+
+from finam import Composition, Info, NoGrid
+from finam.modules.generators import CallbackGenerator
+from finam.modules.visual.schedule import ScheduleView
+
+
+class TestSchedule(unittest.TestCase):
+    def test_schedule(self):
+        start = datetime(2000, 1, 1)
+
+        gen1 = CallbackGenerator(
+            callbacks={"Out": (lambda t: 0, Info(grid=NoGrid()))},
+            start=start,
+            step=timedelta(days=1),
+        )
+        gen2 = CallbackGenerator(
+            callbacks={"Out": (lambda t: 0, Info(grid=NoGrid()))},
+            start=start,
+            step=timedelta(days=3),
+        )
+
+        schedule = ScheduleView(["Gen1", "Gen2"])
+
+        comp = Composition([gen1, gen2, schedule])
+        comp.initialize()
+
+        gen1.outputs["Out"] >> schedule.inputs["Gen1"]
+        gen2.outputs["Out"] >> schedule.inputs["Gen2"]
+
+        comp.run(t_max=datetime(2000, 1, 15))
