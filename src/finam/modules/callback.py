80d1f4b76039b5a1b4b8a634ce717eabce0cb9d6
Martin Lange <martin.lange@ufz.de>
2022-10-24 21:06:30 +0200

    make callback component calculate values at start, add tested example

--- src/finam/modules/callback.py
+++ src/finam/modules/callback.py
@@ -77,6 +77,7 @@ class CallbackComponent(TimeComponent):
         self._callback = callback
         self._step = step
         self._time = start
+        self._data_generated = False
 
     def _initialize(self):
         for name, info in self._input_infos.items():
@@ -87,13 +88,16 @@ class CallbackComponent(TimeComponent):
             info.time = self.time
             self.outputs.add(name=name, info=info)
 
-        self.create_connector()
+        self.create_connector(pull_data=list(self._input_infos))
 
     def _connect(self):
-        inp = {n: None for n in self._input_infos.keys()}
-        outp = self._callback(inp, self.time)
+        push_data = {}
+        if not self._data_generated:
+            if all((data is not None for name, data in self.connector.in_data.items())):
+                push_data = self._callback(self.connector.in_data, self.time)
+                self._data_generated = True
 
-        self.try_connect(self._time, push_data=outp)
+        self.try_connect(self._time, push_data=push_data)
 
     def _validate(self):
         pass
--- tests/modules/test_callback.py
+++ tests/modules/test_callback.py
@@ -4,13 +4,11 @@ from datetime import datetime, timedelta
 import numpy as np
 
 from finam import Composition, Info, NoGrid
-from finam.modules.callback import CallbackComponent
-from finam.modules.generators import CallbackGenerator
+from finam.modules import CallbackComponent, CallbackGenerator, DebugConsumer
 
 
 def transform(inputs, _time):
-    inp = inputs["In1"]
-    return {"Out1": 0.0 if inp is None else inputs["In1"] * 2.0}
+    return {"Out1": inputs["In1"] * 2.0}
 
 
 def consume(_inputs, _time):
@@ -21,7 +19,10 @@ class TestCallback(unittest.TestCase):
     def test_callback(self):
         source = CallbackGenerator(
             callbacks={
-                "Out1": (lambda t: np.random.random(1)[0], Info(None, grid=NoGrid()))
+                "Out1": (
+                    lambda t: 1.0 + np.random.random(1)[0],
+                    Info(None, grid=NoGrid()),
+                )
             },
             start=datetime(2000, 1, 1),
             step=timedelta(days=7),
@@ -35,10 +36,8 @@ class TestCallback(unittest.TestCase):
             step=timedelta(days=7),
         )
 
-        consumer = CallbackComponent(
+        consumer = DebugConsumer(
             inputs={"In1": Info(None, grid=NoGrid())},
-            outputs={},
-            callback=consume,
             start=datetime(2000, 1, 1),
             step=timedelta(days=7),
         )
@@ -49,4 +48,14 @@ class TestCallback(unittest.TestCase):
         _ = source.outputs["Out1"] >> trans.inputs["In1"]
         _ = trans.outputs["Out1"] >> consumer.inputs["In1"]
 
+        composition.connect()
+
+        out_data = consumer.data["In1"]
+        self.assertGreaterEqual(out_data, 2.0)
+        self.assertLessEqual(out_data, 4.0)
+
         composition.run(datetime(2000, 3, 1))
+
+        out_data = consumer.data["In1"]
+        self.assertGreaterEqual(out_data, 2.0)
+        self.assertLessEqual(out_data, 4.0)
