45b5ce9c90ea201dc88f5e7a2226430fd21f08e9
Martin Lange <martin.lange@ufz.de>
2022-10-26 00:44:50 +0200

    remove matplotlib from dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,6 @@ intersphinx_mapping = {
     "Python": ("https://docs.python.org/", None),
     "NumPy": ("http://docs.scipy.org/doc/numpy/", None),
     "SciPy": ("http://docs.scipy.org/doc/scipy/reference", None),
-    "matplotlib": ("http://matplotlib.org", None),
     "xarray": ("https://docs.xarray.dev/en/stable/", None),
     "pint": ("https://pint.readthedocs.io/en/stable/", None),
     "pytest": ("https://docs.pytest.org/en/7.1.x/", None),
