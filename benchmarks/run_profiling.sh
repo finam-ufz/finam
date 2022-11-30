cd96ea17ab8c4402c622b7a8b8a07ff26e572a73
Martin Lange <martin.lange@ufz.de>
2022-11-30 15:12:12 +0100

    convert profiling output to CSV

--- benchmarks/run_profiling.sh
+++ benchmarks/run_profiling.sh
@@ -1,7 +1,10 @@
-echo TEST
+echo Profiling...
 
 mkdir -p prof
+
 python -m cProfile -o prof/simple_run.pstats benchmarks/profiling/simple_run.py
 gprof2dot --colour-nodes-by-selftime -f pstats prof/simple_run.pstats > prof/simple_run.dot
 dot -Tsvg -o prof/simple_run.svg prof/simple_run.dot
 dot -Tpng -o prof/simple_run.png prof/simple_run.dot
+
+python benchmarks/pstats_to_csv.py
