masking: set correct units for nodata in masking adapter

src/finam/adapters/masking.py
@@ -99,6 +99,6 @@ class Masking(Adapter):
     def _transform(self, data):
         if self._canonical_mask is not None:
             data = np.copy(self._sup_grid.to_canonical(data))
-            data[self._canonical_mask] = self.nodata
+            data[self._canonical_mask] = tools.UNITS.Quantity(self.nodata, data.units)
             return self._sub_grid.from_canonical(data)
         return self._sub_grid.from_canonical(self._sup_grid.to_canonical(data))

tests/adapters/test_masking.py
@@ -46,11 +46,11 @@ class TestMasking(unittest.TestCase):
         composition = Composition([source, sink], log_level="DEBUG")
         composition.initialize()
 
-        source.outputs["Output"] >> Masking() >> sink.inputs["Input"]
+        source.outputs["Output"] >> Masking(nodata=-9999) >> sink.inputs["Input"]
 
         composition.connect()
 
-        self.assertTrue(np.isnan(sink.data["Input"][0][0, 0]))
+        self.assertAlmostEqual(sink.data["Input"][0][0, 0].magnitude, -9999)
         self.assertAlmostEqual(sink.data["Input"][0][0, 1].magnitude, 2.0)
 
 