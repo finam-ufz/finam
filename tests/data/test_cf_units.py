cb648b050d75fc48c35e330016ee133b97df809e
Sebastian Müller <mueller.seb@posteo.de>
2024-07-02 00:04:06 +0200

    tests: 'dimensionless' now returned as ''

--- tests/data/test_cf_units.py
+++ tests/data/test_cf_units.py
@@ -23,9 +23,9 @@ class TestCfUnits(unittest.TestCase):
         self.assertEqual(u("degC"), "°C")
         self.assertEqual(u("degree_Celsius"), "°C")
 
-        self.assertEqual(u(""), "dimensionless")
-        self.assertEqual(u("1"), "dimensionless")
-        self.assertEqual(u("m/m"), "dimensionless")
+        self.assertEqual(u(""), "")
+        self.assertEqual(u("1"), "")
+        self.assertEqual(u("m/m"), "")
 
         self.assertEqual(u("m/s"), "m s-1")
         self.assertEqual(u("m s-1"), "m s-1")
